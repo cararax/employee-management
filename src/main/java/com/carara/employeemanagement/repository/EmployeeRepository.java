075dd66a551919e59568c50ff58d76038c0f29e0
Pedro Carara <cararax@gmail.com>
2022-12-11 16:48:28 -0300

    fix: typo no repository de employee

--- src/main/java/com/carara/employeemanagement/repository/EmployeeRepository.java
+++ src/main/java/com/carara/employeemanagement/repository/EmployeeRepository.java
@@ -3,5 +3,5 @@ package com.carara.employeemanagement.repository;
 import com.carara.employeemanagement.domain.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface employeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
