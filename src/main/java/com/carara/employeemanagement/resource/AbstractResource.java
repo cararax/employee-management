68e68e8dcf1b991e3462a06878d46c4d3dc7aee1
Pedro Carara <cararax@gmail.com>
2022-12-11 16:36:54 -0300

    feat: service e controller de abstractEntity

---
+++ src/main/java/com/carara/employeemanagement/resource/AbstractResource.java
@@ -0,0 +1,62 @@
+package com.carara.employeemanagement.resource;
+
+import com.carara.employeemanagement.mapper.AbstractMapper;
+import com.carara.employeemanagement.service.AbstractService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@AllArgsConstructor
+public class AbstractResource<D, T, I, ID, M extends AbstractMapper<D, T, I>, R extends JpaRepository<D, ID>, S extends AbstractService<D, T, I, ID, M, R>> {    protected S service;
+
+    @PostMapping
+    public ResponseEntity<T> create(@RequestBody I i) throws Exception {
+
+        var response = service.save(i);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<T> findByCode(@PathVariable ID id) {
+        var response = service.findById(id);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<T>>findAllPage(
+            @PageableDefault(sort = "id",
+                    direction = Sort.Direction.ASC,
+                    page = 0,
+                    size = 10) Pageable page) {
+
+        var pages = service.findAllPage(page);
+        return ResponseEntity.ok().body(pages);
+    }
+
+    @GetMapping(value ="/all")
+    public ResponseEntity<List<T>> findAll() {
+
+        var persons = service.findAll();
+        return ResponseEntity.ok().body(persons);
+    }
+
+    @PutMapping(value ="/{id}")
+    public ResponseEntity<T> update(@PathVariable ID id, @RequestBody I i) throws Exception {
+
+        var response = service.update(id, i);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @DeleteMapping(value ="/{id}")
+    public ResponseEntity<Void> delete(@PathVariable ID id){
+        service.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/carara/employeemanagement/service/AbstractService.java
@@ -0,0 +1,60 @@
+package com.carara.employeemanagement.service;
+
+import com.carara.employeemanagement.exception.ObjectNotFoundException;
+import com.carara.employeemanagement.mapper.AbstractMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@AllArgsConstructor
+public abstract class AbstractService<D, T, I, ID, M extends AbstractMapper<D, T, I>, R extends JpaRepository<D, ID>> {
+
+    protected R repository;
+    protected M mapper;
+
+    public List<T> findAll(){
+        final List<D> list = this.repository.findAll();
+        return mapper.parserToListResponse(list);
+    }
+
+    public Page<T> findAllPage(Pageable pageable) {
+        final Page<D> pages = repository.findAll(pageable);
+        return mapper.parserToPageResponse(pages);
+    }
+
+    public T findById(ID id){
+        return mapper.parseResponse(exist(id));
+    }
+
+    @Transactional
+    public T save(I i) throws Exception {
+        D domain = mapper.parseDomain(i);
+        domain = this.repository.save(domain);
+        return mapper.parseResponse(domain);
+    }
+
+    @Transactional
+    public T update(ID id, I i) throws Exception {
+        D d = exist(id);
+        BeanUtils.copyProperties(i, d, "id");
+        d = this.repository.save(d);
+        return mapper.parseResponse(d);
+    }
+
+    @Transactional
+    public void delete(ID id) {
+        D d = exist(id);
+        repository.delete(d);
+    }
+
+    private D exist(ID id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(String.format("Not found regitstry with code %d", id)));
+    }
+
+}
